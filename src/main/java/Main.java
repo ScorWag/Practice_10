5e6add47705849926448d3f282a8a8712882b2dd
ScorWag <scorwag@rambler.ru>
2022-06-12 09:45:35 +0300

    Init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import ru.netology.domain.MovieItem;
+import ru.netology.manager.MovieManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        MovieItem movieFirst = new MovieItem(1, "Terminator", "Action");
+//        MovieItem movieSecond = new MovieItem(2, "Forrest Gump", "Drama");
+//        MovieItem movieThird = new MovieItem(3, "The Mask", "Comedy");
+//        MovieItem movieFour = new MovieItem(4, "Terminator", "Action");
+//        MovieItem movieFive = new MovieItem(5, "Forrest Gump", "Drama");
+//        MovieItem movieSix = new MovieItem(6, "The Mask", "Comedy");
+//        MovieItem movieSeven = new MovieItem(7, "Terminator", "Action");
+//        MovieItem movieEight = new MovieItem(8, "Forrest Gump", "Drama");
+//        MovieItem movieNine = new MovieItem(9, "The Mask", "Comedy");
+//        MovieItem movieTen = new MovieItem(10, "Terminator", "Action");
+//        MovieItem movieEleven = new MovieItem(11, "Forrest Gump", "Drama");
+
+//        MovieManager manager = new MovieManager(5);
+//        manager.add(movieFirst);
+//        manager.add(movieSecond);
+//        manager.add(movieThird);
+//        manager.add(movieFour);
+//        manager.add(movieFive);
+//        manager.add(movieSix);
+//        manager.add(movieSeven);
+//        manager.add(movieEight);
+//        manager.add(movieNine);
+//        manager.add(movieTen);
+//        manager.add(movieEleven);
+
+//        MovieItem[] films = manager.findAll();
+//        for (int i = 0; i < films.length; i++) {
+//            System.out.println(films[i]);
+//        }
+    }
+}
---
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -0,0 +1,47 @@
+package ru.netology.manager;
+
+import ru.netology.domain.MovieItem;
+
+public class MovieManager {
+    private MovieItem[] movies = new MovieItem[0];
+    private int maxLength = 10;
+
+
+    public MovieManager() {
+        }
+
+    public MovieManager(int maxLength) {
+        this.maxLength = maxLength;
+        }
+    public void add(MovieItem item) {
+
+        MovieItem[] tmp = new MovieItem[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = item;
+        movies = tmp;
+    }
+    public MovieItem[] findAll() {
+        return movies;
+    }
+
+    public MovieItem[] findLast() {
+        int resultLength;
+
+        if (movies.length >= maxLength) {
+            resultLength = maxLength;
+            }
+        else {
+            resultLength = movies.length;
+        }
+        MovieItem[] items = findAll();
+        MovieItem[] result = new MovieItem[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+}
